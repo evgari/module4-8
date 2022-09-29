8c35427343b6d0bd1943ffa6b14329b3c91d0b6c
Evgeniy Oshepkov <antarievg@yandex.ru>
2022-09-29 22:57:22 +0300

    fixed

--- task03.js
+++ task03.js
@@ -9,25 +9,20 @@ const generateArrRandNums = (count, m , n, str) => {
 
   if (str) {
     const newArr = arr.map(item => {
-      if (str === 'odd' || str === 'even') {
-        if ((str === 'odd' && item % 2) || (str === 'even' && !(item % 2))) return item;
-        if (item !== Math.min(m, n)) {
-          return item - 1;
-        } else {
-          return item + 1;
-        }
+      if (str !== 'odd' && str !== 'even') return item;
+      if ((str === 'odd' && item % 2) || (str === 'even' && !(item % 2))) return item;
+      if (item !== Math.min(m, n)) {
+        return item - 1;
       } else {
-        return item;
-      }  
+        return item + 1;
+      }
     });
 
     return newArr;
-
   } else {
     return arr;
   }
 };
-
 
 const generateRandNum = (m, n) => {
   let range = Math.abs(m - n);
@@ -37,7 +32,6 @@ const generateRandNum = (m, n) => {
 
   return randNum;
 };
-
 
 console.log(generateArrRandNums(20, 1, 10));
 console.log(generateArrRandNums(20, 0, 10, 'aasof'));
